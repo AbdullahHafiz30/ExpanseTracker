98b800419f7ba908a400b2f26287640f19ba1100
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-03-27 12:02:32 +0300

    Initial commit

---
+++ ExpanseTracker/ExpanseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpanseTrackerApp.swift
+//  ExpanseTracker
+//
+//  Created by Abdullah Hafiz on 27/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ExpanseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
