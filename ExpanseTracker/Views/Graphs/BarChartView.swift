3c65411160f8bfa62f47b12aa668d5fac458a82a
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-04-13 10:18:19 +0300

    Graphs Files
    added Pie, Bar and Line View (didn't do anything yet)

---
+++ ExpanseTracker/Views/Graphs/BarChartView.swift
@@ -0,0 +1,7 @@
+//
+//  BarChartView.swift
+//  ExpanseTracker
+//
+//  Created by Abdullah Hafiz on 15/10/1446 AH.
+//
+
---
+++ ExpanseTracker/Views/Graphs/PieChartView.swift
@@ -0,0 +1,30 @@
+//
+//  PieChartView.swift
+//  ExpanseTracker
+//
+//  Created by Abdullah Hafiz on 15/10/1446 AH.
+//
+
+import SwiftUI
+
+struct PieChartView: View {
+    
+    @EnvironmentObject var themeManager: ThemeManager
+    @State private var showDatePicker = false
+    @State private var date = Date()
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                
+                VStack {
+                    HStack {
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
+
