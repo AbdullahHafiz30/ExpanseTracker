b169de119507bd13b8034d734f86e474a528633b
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-04-15 14:46:39 +0300

    added charts files
    added charts files and dummy data

--- ExpanseTracker/Models/Transactions.swift
+++ ExpanseTracker/Models/Transactions.swift
@@ -7,14 +7,36 @@
 
 import Foundation
 
+<<<<<<< Updated upstream
 struct Transactions: Identifiable,Hashable {
     let id: UUID?
     let titleq: String?
+=======
+struct Transactions: Identifiable {
+    let id: String?
+    let title: String?
+>>>>>>> Stashed changes
     let description: String?
     let amount: Double?
     let date: Date?
     let transactionType: TransactionType?
+<<<<<<< Updated upstream
     let category: String
     let receiptImage: String?
+=======
+    let category: Category?
+    let receiptImage: String? // tahani added
+    
+    init(id: String?, title: String?, description: String?, amount: Double?, date: Date?, transactionType: TransactionType?, category: Category?, receiptImage: String?) {
+        self.id = id
+        self.title = title
+        self.description = description
+        self.amount = amount
+        self.date = date
+        self.transactionType = transactionType
+        self.category = category
+        self.receiptImage = receiptImage
+    }
+>>>>>>> Stashed changes
 }
 
---
+++ ExpanseTracker/Views/Graphs/LineView.swift
@@ -0,0 +1,14 @@
+//
+//  LineView.swift
+//  ExpanseTracker
+//
+//  Created by Abdullah Hafiz on 15/04/2025.
+//
+
+import SwiftUI
+
+struct LineView: View {
+    var body: some View {
+        Text("Line View")
+    }
+}
