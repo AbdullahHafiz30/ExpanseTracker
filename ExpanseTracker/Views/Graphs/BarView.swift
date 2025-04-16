b3044a8feceaf114a46c8ada948f6e01eb648ef6
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-04-16 14:36:23 +0300

    added bar chart example

--- ExpanseTracker/Views/Graphs/BarView.swift
+++ ExpanseTracker/Views/Graphs/BarView.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+import Charts
 
 struct BarView: View {
+    
+    @ObservedObject var viewModel = DummyDataView()
+    
     var body: some View {
-        Text("Bar View")
+        
+        VStack {
+            Chart(viewModel.getTestData()) {
+                BarMark(
+                    x: .value("category", $0.text),
+                    y: .value("expense", $0.number)
+                ).foregroundStyle(by: .value("Product Category", $0.text))
+                
+            }
+        }
     }
 }
